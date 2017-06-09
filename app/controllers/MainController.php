0d0dd4f572d7585b464ae11c219ded89d940f327
Jorge Felico <jorgefelico@gmail.com>
2017-06-09 08:49:52 -0400

    More updates

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -6,12 +6,13 @@ class MainController {
   protected $container;
   protected $view;
 
-  public function __contruct(ContainerInterface $container){
+  public function __construct($container){
     $this->container = $container;
   }
 
   public function home($request, $response, $args){
     $this->view = $this->container->get("view");
+
     return $this->view->render($response, 'home.html');
   }
 
